db7ef6fac5b8293ce508cc172de4df5e43b483ce
Prem Shah <premparag@gmail.com>
2018-02-19 18:39:09 -0800

    Create kthLargestElementinArray.py

---
+++ Programming/kthLargestElementinArray.py
@@ -0,0 +1,23 @@
+from heapq import *
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        return(nums[len(nums)-k])
+        
+    def findKthLargest2(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heap = []
+        for num in nums:
+            heappush(heap,num)
+        
+        return nlargest(k,heap)[k-1]
+        
